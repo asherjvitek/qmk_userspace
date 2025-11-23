63bec9039507ce7552ee8989800691771e7d17ae
ashervitek <asher.vitek@gmail.com>
2025-11-23 06:59:55 -0600

    Changing the button clicks to better buttons. I just do not like the lower buttons on the thumbs.

--- keyboards/bastardkb/charybdis/4x6/keymaps/vendor/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/vendor/keymap.c
@@ -47,20 +47,21 @@ enum charybdis_keymap_layers {
     LAYER_GAME,
     LAYER_NAV,
     LAYER_FUN,
-    LAYER_NUM,
+    // LAYER_NUM,
     LAYER_POINTER,
 };
 
 #define LT_LP_KC_SLASH LT(LAYER_POINTER, KC_SLSH)
+#define LT_LP_KC_Z LT(LAYER_POINTER, KC_Z)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_BASE] = LAYOUT(
     KC_EQUAL,       KC_1,               KC_2,               KC_3,               KC_4,               KC_5,                           KC_6,                   KC_7,                   KC_8,               KC_9,               KC_0,                  KC_MINUS,
     KC_TAB,         KC_Q,               KC_W,               KC_E,               KC_R,               KC_T,                           KC_Y,                   KC_U,                   KC_I,               KC_O,               KC_P,                  KC_BSLS,
     KC_ESCAPE,      MT(MOD_LGUI, KC_A), MT(MOD_LALT, KC_S), MT(MOD_LSFT, KC_D), MT(MOD_LCTL, KC_F), KC_G,                           KC_H,                   MT(MOD_RCTL, KC_J),     MT(MOD_RSFT, KC_K), MT(MOD_RALT, KC_L), MT(MOD_RGUI, KC_SCLN), KC_QUOTE,
-    KC_LSFT,        KC_Z,               KC_X,               KC_C,               KC_V,               KC_B,                           KC_N,                   KC_M,                   KC_COMMA,           KC_DOT,             LT_LP_KC_SLASH,        KC_LSFT,
-                                                            KC_BTN1,            KC_SPACE,           LT(LAYER_NUM, KC_DELETE),       LT(LAYER_FUN, KC_BSPC), LT(LAYER_NAV, KC_ENTER),
-                                                            KC_BTN3,            KC_BTN2,                                            TG(LAYER_GAME)
+    KC_LSFT,        LT_LP_KC_Z,         KC_X,               KC_C,               KC_V,               KC_B,                           KC_N,                   KC_M,                   KC_COMMA,           KC_DOT,             LT_LP_KC_SLASH,        KC_LSFT,
+                                                            KC_BTN1,            KC_SPACE,           KC_DELETE,                      LT(LAYER_FUN, KC_BSPC), LT(LAYER_NAV, KC_ENTER),
+                                                            XXXXXXX,            XXXXXXX,                                            TG(LAYER_GAME)
   ),
   [LAYER_GAME] = LAYOUT(
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
@@ -86,21 +87,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT,
                                                     KC_TRANSPARENT, KC_TRANSPARENT,                                                 KC_TRANSPARENT
   ),
-  [LAYER_NUM] = LAYOUT(
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_SLASH,       KC_ASTR,        KC_MINUS,       KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_7,           KC_8,           KC_9,           KC_PLUS,        KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_LEFT_GUI,    KC_LEFT_ALT,    KC_LEFT_SHIFT,  KC_LEFT_CTRL,   KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_6,           KC_5,           KC_4,           KC_ENTER,       KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_1,           KC_2,           KC_3,           KC_ENTER,       KC_TRANSPARENT,
-                                                    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_0,
-                                                    KC_TRANSPARENT, KC_TRANSPARENT,                                                 KC_TRANSPARENT
-  ),
+  // [LAYER_NUM] = LAYOUT(
+  //   KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_SLASH,       KC_ASTR,        KC_MINUS,       KC_TRANSPARENT,
+  //   KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_7,           KC_8,           KC_9,           KC_PLUS,        KC_TRANSPARENT,
+  //   KC_TRANSPARENT, KC_LEFT_GUI,    KC_LEFT_ALT,    KC_LEFT_SHIFT,  KC_LEFT_CTRL,   KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_6,           KC_5,           KC_4,           KC_ENTER,       KC_TRANSPARENT,
+  //   KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_1,           KC_2,           KC_3,           KC_ENTER,       KC_TRANSPARENT,
+  //                                                   KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_0,
+  //                                                   KC_TRANSPARENT, KC_TRANSPARENT,                                                 KC_TRANSPARENT
+  // ),
   [LAYER_POINTER] = LAYOUT(
        QK_BOOT,  EE_CLR, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                                                        XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, QK_BOOT,  EE_CLR,
        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                                                        XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,
        XXXXXXX, KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, XXXXXXX,                                                                        XXXXXXX, KC_RCTL, KC_RSFT,  KC_RALT, KC_RGUI, XXXXXXX,
        XXXXXXX, XXXXXXX, DRGSCRL, SNP_TOG, S_D_MOD, S_D_RMOD,                                                                       XXXXXXX, XXXXXXX, DPI_MOD, DPI_RMOD, XXXXXXX, XXXXXXX,
-                                  _______, _______, _______,                                                                        XXXXXXX, XXXXXXX,
-                                           _______, _______,                                                                        XXXXXXX
+                                  KC_BTN1, KC_BTN3, KC_BTN2,                                                                        XXXXXXX, XXXXXXX,
+                                           XXXXXXX, XXXXXXX,                                                                        XXXXXXX
   ),
 };
 
